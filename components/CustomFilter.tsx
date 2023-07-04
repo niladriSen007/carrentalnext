09642cc474a3a51ce9d4bd03acdb55ea48763ecc
Niladri <niladrisengit12@gmail.com>
2023-07-04 09:08:42 +0530

    Filter section part 1

---
+++ components/CustomFilter.tsx
@@ -0,0 +1,10 @@
+import { FilterTitle } from '@/types'
+import React from 'react'
+
+const CustomFilter = ({title}:FilterTitle) => {
+  return (
+    <div>CustomFilter</div>
+  )
+}
+
+export default CustomFilter
